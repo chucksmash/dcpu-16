a7c0d7f4a9638d92adafc40eeddafe10239466bd
Chuck Bassett <3101367+chucksmash@users.noreply.github.com>
2018-11-23 11:57:22 -0500

    More complicated bin/main

--- assembler/src/bin/main.rs
+++ assembler/src/bin/main.rs
@@ -1,17 +1,31 @@
 extern crate assembler;
 
+use assembler::codegen;
 use assembler::lexer;
 use assembler::parser;
 
 fn main() {
-    let input = "ADD    [0x21], 0b101
-SET B   ,     123
-jsr [11]
-   set c ,0b11
+    let input = "JSR 12
+
+; welcome message
+chuck: SET X, 0
+made: this:
+SET Y, 3
+SET A, 0
+
+
+happen:
+SET X, 0    ; change colors
+SET Y, 0xf
+SET A, 1
+
+SET I, 0    ; counter for device_draw_loop
 ";
     let lexed_lines = lexer::lex(input).unwrap();
     let parsed = parser::parse(&mut lexed_lines.iter()).unwrap();
     for line in parsed.get_lines().iter() {
         println!("{:?}", line);
     }
+    let code = codegen::generate_code(parsed);
+    println!("{}", code);
 }
